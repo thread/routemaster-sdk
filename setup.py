1300508525a3e1495c640f9c7ff689bb6f621d7e
Peter Law <PeterJCLaw@gmail.com>
2019-11-06 11:28:28 +0000

    Declare support for Python 3.7 & 3.8

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Office/Business',
     ),
 
